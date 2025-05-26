dbcac04127cc71b2549125b0eb14028734130902
Ylva Fröjmark <ylva.frojamrk@gmail.com>
2025-05-26 17:14:11 +0200

    CustomerServiceImpltest2: knuten till databas, @BeforeEach-metod, skelett till övriga metoder. (Gjorde denna för att jag är osäker på om funktionerna i den är kompatibla med den andra testfilen och ville inte förstöra i den andra?

--- src/main/java/org/spring/theguesthouse/TheGuesthouseApplication.java
+++ src/main/java/org/spring/theguesthouse/TheGuesthouseApplication.java
@@ -19,65 +19,65 @@ public class TheGuesthouseApplication {
     public static void main(String[] args) {
         SpringApplication.run(TheGuesthouseApplication.class, args);
     }
-//        @Bean
-//    public CommandLineRunner commandLineRunner(BookingRepo bookingRepo, CustomerRepo customerRepo, RoomRepo roomRepo) {
-//        return args -> {
-//
-//            Room r1 = Room.builder().roomNumber(101).build();
-//            Room r2 = Room.builder().roomNumber(102).build();
-//            Room r3 = Room.builder().roomNumber(103).build();
-//            Room r4 = Room.builder().roomNumber(201).build();
-//            Room r5 = Room.builder().roomNumber(202).build();
-//            Room r6 = Room.builder().roomNumber(203).build();
-//            Room r7 = Room.builder().roomNumber(301).build();
-//            Room r8 = Room.builder().roomNumber(302).build();
-//            Room r9 = Room.builder().roomNumber(303).build();
-//            Room r10 = Room.builder().roomNumber(1337).build();
-//
-//            roomRepo.save(r1);
-//            roomRepo.save(r2);
-//            roomRepo.save(r3);
-//            roomRepo.save(r4);
-//            roomRepo.save(r5);
-//            roomRepo.save(r6);
-//            roomRepo.save(r7);
-//            roomRepo.save(r8);
-//            roomRepo.save(r9);
-//            roomRepo.save(r10);
-//
-//            Customer c1 = Customer.builder().name("Maja Gräddnos").tel("018-225162").build();
-//            Customer c2 = Customer.builder().name("Gammel-Maja").tel("018-225163").build();
-//            Customer c3 = Customer.builder().name("Gullan von Arkadien").tel("018-225164").build();
-//            Customer c4 = Customer.builder().name("Laban i Observatorielunden").tel("018-225165").build();
-//            Customer c5 = Customer.builder().name("Sitting Bill").tel("018-225166").build();
-//
-//            customerRepo.save(c1);
-//            customerRepo.save(c2);
-//            customerRepo.save(c3);
-//            customerRepo.save(c4);
-//            customerRepo.save(c5);
-//
-//           Booking b1 = Booking.builder().customer(c1)
-//                   .startDate(LocalDate.of(2025, 10, 15))
-//                   .endDate(LocalDate.of(2025, 10, 18)).room(r1).build();
-//
-//           Booking b2 = Booking.builder().customer(c1)
-//                    .startDate(LocalDate.of(2025, 10, 2))
-//                    .endDate(LocalDate.of(2025, 10, 3)).room(r2).build();
-//
-//           Booking b3 = Booking.builder().customer(c3)
-//                    .startDate(LocalDate.of(2025, 8, 15))
-//                    .endDate(LocalDate.of(2025, 9, 1)).room(r3).build();
-//
-//           Booking b4 = Booking.builder().customer(c2)
-//                    .startDate(LocalDate.of(2025, 8, 16))
-//                    .endDate(LocalDate.of(2025, 8, 18)).room(r4).build();
-//
-//           bookingRepo.save(b1);
-//           bookingRepo.save(b2);
-//           bookingRepo.save(b3);
-//           bookingRepo.save(b4);
-//
-//        };
-//    }
+        @Bean
+    public CommandLineRunner commandLineRunner(BookingRepo bookingRepo, CustomerRepo customerRepo, RoomRepo roomRepo) {
+        return args -> {
+
+            Room r1 = Room.builder().roomNumber(101).build();
+            Room r2 = Room.builder().roomNumber(102).build();
+            Room r3 = Room.builder().roomNumber(103).build();
+            Room r4 = Room.builder().roomNumber(201).build();
+            Room r5 = Room.builder().roomNumber(202).build();
+            Room r6 = Room.builder().roomNumber(203).build();
+            Room r7 = Room.builder().roomNumber(301).build();
+            Room r8 = Room.builder().roomNumber(302).build();
+            Room r9 = Room.builder().roomNumber(303).build();
+            Room r10 = Room.builder().roomNumber(1337).build();
+
+            roomRepo.save(r1);
+            roomRepo.save(r2);
+            roomRepo.save(r3);
+            roomRepo.save(r4);
+            roomRepo.save(r5);
+            roomRepo.save(r6);
+            roomRepo.save(r7);
+            roomRepo.save(r8);
+            roomRepo.save(r9);
+            roomRepo.save(r10);
+
+            Customer c1 = Customer.builder().name("Maja Gräddnos").tel("018-225162").build();
+            Customer c2 = Customer.builder().name("Gammel-Maja").tel("018-225163").build();
+            Customer c3 = Customer.builder().name("Gullan von Arkadien").tel("018-225164").build();
+            Customer c4 = Customer.builder().name("Laban i Observatorielunden").tel("018-225165").build();
+            Customer c5 = Customer.builder().name("Sitting Bill").tel("018-225166").build();
+
+            customerRepo.save(c1);
+            customerRepo.save(c2);
+            customerRepo.save(c3);
+            customerRepo.save(c4);
+            customerRepo.save(c5);
+
+           Booking b1 = Booking.builder().customer(c1)
+                   .startDate(LocalDate.of(2025, 10, 15))
+                   .endDate(LocalDate.of(2025, 10, 18)).room(r1).build();
+
+           Booking b2 = Booking.builder().customer(c1)
+                    .startDate(LocalDate.of(2025, 10, 2))
+                    .endDate(LocalDate.of(2025, 10, 3)).room(r2).build();
+
+           Booking b3 = Booking.builder().customer(c3)
+                    .startDate(LocalDate.of(2025, 8, 15))
+                    .endDate(LocalDate.of(2025, 9, 1)).room(r3).build();
+
+           Booking b4 = Booking.builder().customer(c2)
+                    .startDate(LocalDate.of(2025, 8, 16))
+                    .endDate(LocalDate.of(2025, 8, 18)).room(r4).build();
+
+           bookingRepo.save(b1);
+           bookingRepo.save(b2);
+           bookingRepo.save(b3);
+           bookingRepo.save(b4);
+
+        };
+    }
 }
